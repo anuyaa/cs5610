01527c71a07b9abfccaef83bf8b0a08099f12ae8
anuyaa <ankitaa77777@gmail.com>
2014-10-09 15:42:02 -0400

    get environment variables

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 //  OpenShift sample Node application
+// load web service and database libraries
 var express = require('express');
+//var mongo = require('mongojs')
 
 //instantiates both libraries and connect to database to cs5610
 var app = express();
+//var db = mongojs("cs5610",["student","professor"]);
+app.use(expres.static(_dirname+'/public'));
 
 app.get("/hello", function(req, res){
 	res.send("Hello world from open shift application ");
 });
 
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+app.get("/env", function(req, res){
+	res.json(process.env);
+});
+
+
+
+var ipaddress =  process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port      =  process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
 
 app.listen(port,ipaddress);
