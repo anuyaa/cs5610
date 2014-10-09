740dca21b5f789ca07644a979f8af0ac1e596dd8
anuyaa <ankitaa77777@gmail.com>
2014-10-09 15:54:14 -0400

    added mongojs

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 //  OpenShift sample Node application
 // load web service and database libraries
 var express = require('express');
-//var mongo = require('mongojs')
+var mongojs = require('mongojs')
 
 //instantiates both libraries and connect to database to cs5610
 var app = express();
-//var db = mongojs("cs5610",["student","professor"]);
+
+var mongodbConnectionString = process.env.OPENSHIFT_MONGODB_DB_URL + "cs5610DB"
+if(typeof process.env.OPENSHIFT_MONGODB_DB_URL == "undefined"){
+	mongodbConnectionString = "cs5610DB";
+}
+var db = mongojs(mongodbConnectionString,["student","professor"]);
 //app.use(express.static(_dirname+'/public'));
 
 app.get("/hello", function(req, res){
